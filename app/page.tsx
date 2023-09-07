9fc418403ba4a89bca39b3c0bed6974a8e48a40f
Tuan Tran <tranvantuanit210@gmail.com>
2023-09-08 00:07:23 +0700

    Check window

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 "use client";
 
 import { QRCodeSVG } from "qrcode.react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [baseUrl, setBaseUrl] = useState("");
+
+  useEffect(() => {
+    // Check if window is defined (i.e., running in the browser)
+    if (typeof window !== "undefined") {
+      // Access window.location
+      const currentLocation = window.location;
+      setBaseUrl(currentLocation.origin);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <main className="min-h-screen grid place-items-center place-content-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 p-6">
       <div className="flex flex-col gap-4 items-center">
         <span className="text-lg font-bold">Page 1</span>
         <QRCodeSVG
-          value={`${window.location.origin}/page1`}
+          value={`${baseUrl}/page1`}
           className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]"
           bgColor={"#ffffff"}
           fgColor={"#000000"}
@@ -19,7 +32,7 @@ export default function Home() {
       <div className="flex flex-col gap-4 items-center">
         <span className="text-lg font-bold">Page 2</span>
         <QRCodeSVG
-          value={`${window.location.origin}/page2`}
+          value={`${baseUrl}/page2`}
           className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]"
           bgColor={"#ffffff"}
           fgColor={"#000000"}
@@ -30,7 +43,7 @@ export default function Home() {
       <div className="flex flex-col gap-4 items-center">
         <span className="text-lg font-bold">Page 3</span>
         <QRCodeSVG
-          value={`${window.location.origin}/page3`}
+          value={`${baseUrl}/page3`}
           className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]"
           bgColor={"#ffffff"}
           fgColor={"#000000"}
