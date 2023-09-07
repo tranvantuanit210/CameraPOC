d3e72a81c840e8c1b4bdf153f19efa622cdb7203
Tuan Tran <tranvantuanit210@gmail.com>
2023-09-07 23:41:08 +0700

    Add use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { QRCodeSVG } from "qrcode.react";
 
 export default function Home() {
@@ -6,7 +8,7 @@ export default function Home() {
       <div className="flex flex-col gap-4 items-center">
         <span className="text-lg font-bold">Page 1</span>
         <QRCodeSVG
-          value={`${process.env.NEXT_PUBLIC_BASE_URL}/page1`}
+          value={`${window.location.origin}/page1`}
           className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]"
           bgColor={"#ffffff"}
           fgColor={"#000000"}
@@ -17,7 +19,7 @@ export default function Home() {
       <div className="flex flex-col gap-4 items-center">
         <span className="text-lg font-bold">Page 2</span>
         <QRCodeSVG
-          value={`${process.env.NEXT_PUBLIC_BASE_URL}/page2`}
+          value={`${window.location.origin}/page2`}
           className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]"
           bgColor={"#ffffff"}
           fgColor={"#000000"}
@@ -28,7 +30,7 @@ export default function Home() {
       <div className="flex flex-col gap-4 items-center">
         <span className="text-lg font-bold">Page 3</span>
         <QRCodeSVG
-          value={`${process.env.NEXT_PUBLIC_BASE_URL}/page3`}
+          value={`${window.location.origin}/page3`}
           className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]"
           bgColor={"#ffffff"}
           fgColor={"#000000"}
